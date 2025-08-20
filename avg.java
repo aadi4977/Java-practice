d9044c6a0fde9403ffbf38f491d0459fab95e666
aadi4977 <aadigupta41@gmail.com>
2025-08-20 23:56:10 +0530

    First commit

---
+++ avg.java
@@ -0,0 +1,10 @@
+public class avg {
+    public static void main(String[] args) {
+        String name = "Sam";
+        int maths = 94;
+        int physics =95;
+        int chemsitry =96;
+        int avg=(maths+physics+chemsitry)/3;
+        System.out.println("Avg of his marks are :"+avg);
+    }
+}
