b5b362227d25dc583b50214ab9a19ace912c7642
aadi4977 <aadigupta41@gmail.com>
2025-08-24 02:03:39 +0530

    Add files via upload

---
+++ 123/earth.java
@@ -0,0 +1,24 @@
+public class earth {
+    public static void main(String[] args) {
+         double radiusKm = 6378; 
+        double kmToMiles = 0.621371; 
+
+        
+        double volumeKm3 = (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+
+        
+        double radiusMiles = radiusKm * kmToMiles;
+        double volumeMiles3 = (4.0 / 3.0) * Math.PI * Math.pow(radiusMiles, 3);
+
+        System.out.println("The volume of Earth in cubic kilometers is " + volumeKm3);
+        System.out.println("The volume of Earth in cubic miles is " + volumeMiles3);
+
+        
+        double distanceKm = 10; 
+        double distanceMiles = distanceKm * kmToMiles;
+
+        System.out.println(distanceKm + " kilometers is equal to " + distanceMiles + " miles.");
+    }
+    
+    
+}
---
+++ 123/hello.java
@@ -0,0 +1,14 @@
+
+ public class hello{
+    public static void main(String[] args) {
+        String name  = "Harry";
+        int birthyear = 2000;
+        int currentyear = 2024;
+        
+        int age = currentyear - birthyear;
+        System.out.println("Age of " + name + " is: " + age);
+        
+    }
+ }
+
+ 
